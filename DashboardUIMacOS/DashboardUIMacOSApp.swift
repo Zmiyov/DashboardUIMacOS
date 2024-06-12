091c17f6de6f78ea6008e5c7807ef483b643f66e
Volodymyr <zmiyov88@gmail.com>
2024-06-12 15:53:42 +0300

    Initial Commit

---
+++ DashboardUIMacOS/DashboardUIMacOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  DashboardUIMacOSApp.swift
+//  DashboardUIMacOS
+//
+//  Created by Volodymyr Pysarenko on 12.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct DashboardUIMacOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
